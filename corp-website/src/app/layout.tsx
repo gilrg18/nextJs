98a5a535086fd2a542215bbc1b1ecc9b7bfec4ed
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-10-17 00:20:59 -0700

    Common UI in Next JS with Layouts

--- corp-website/src/app/layout.tsx
+++ corp-website/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,8 +29,18 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div>
+          <Link href="/">Home</Link>
+          <Link href="/performance">Performance</Link>
+          <Link href="/reliability">Reliability</Link>
+          <Link href="/scale">Scale</Link>
+        </div>
         {children}
+        {/* children are the components that we will render such as PerformancePage, ReliabilityPage, etc... */}
       </body>
     </html>
   );
 }
+//Layout is a global parent component that is used for all of our different page components
+//when we access f.e. /performance this is what happens bts:
+//<Layout> <PerformancePage /> </Layout>
