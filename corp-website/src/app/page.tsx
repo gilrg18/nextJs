a98c510c05e5d2d5be9b758ee1e642dae1e7371f
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-10-18 11:50:12 -0700

    More on the Image Component

--- corp-website/src/app/page.tsx
+++ corp-website/src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import homeImg from "public/home.jpg";
+
+
 export default function Home() {
   return (
     <div>
       Home Page
-      <div className="absolute -z-10 inset-0">
-        {" "}
+      <div className="absolute -z-10 inset-0">      
         <Image
           src={homeImg}
           alt="car factory"
@@ -16,3 +17,15 @@ export default function Home() {
     </div>
   );
 }
+//Image component helps avoid 'Layout/Content shifting' (when components move around depending on what loads first) 
+//[Test slow connection/slow load times in browser]: f2 -> network -> tick 'Disable cache' -> Slow 4G
+
+//Options for sizing the image:
+//A. If using a local image, dimensions are taken from the imported image
+console.log(homeImg); //Check image size, location, etc
+
+//B. Assign a height and width to the image component
+{/* <Image height={600} width={800} src = {homeImg} alt='asdf' /> */}
+
+//C. Assign a'fill' prop, the image will expand to fill up the parent element.
+{/* <Image fill src = {homeImg} alt='asdf' /> */}
