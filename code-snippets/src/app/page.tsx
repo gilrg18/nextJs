16e81ea289164a37892afce7b7b2d79c5f8f72e7
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-10-24 21:51:21 -0700

    Fetching Data with Server Components

--- code-snippets/src/app/page.tsx
+++ code-snippets/src/app/page.tsx
@@ -1,7 +1,10 @@
+import { db } from "@/db";
 
+export default async function Home() {
+  const snippets = await db.snippet.findMany(); //get all the snippets from db.
 
-export default function Home() {
-  return (
-    <div>HomePage</div>
-  );
+  const renderedSnippets = snippets.map((snippet) => {
+    return <div key={snippet.id}>{snippet.title}</div>;
+  });
+  return <div>{renderedSnippets}</div>;
 }
