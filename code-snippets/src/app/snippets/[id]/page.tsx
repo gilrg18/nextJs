5b5a70856578e6517b233e42196f41428bdc9494
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-10-24 22:23:22 -0700

    Fetching Particular Records

--- code-snippets/src/app/snippets/[id]/page.tsx
+++ code-snippets/src/app/snippets/[id]/page.tsx
@@ -1,4 +1,23 @@
-export default function SnippetShowPage(props: any){
-    console.log(props);
-    return <div>Showing a snippet</div>
+import { notFound } from "next/navigation";
+import { db } from "@/db";
+
+//interface to describe the structure of our props
+interface SnippetShowPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  const { id } = await props.params;
+
+  const snippet = await db.snippet.findFirst({
+    where: { id: parseInt(id) },
+  });
+
+  if (!snippet) {
+    return notFound();
+  }
+
+  return <div>{snippet.title}</div>;
 }
